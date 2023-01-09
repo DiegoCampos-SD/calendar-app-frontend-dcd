2f4851fed414e1c5a3a76b05af96be1b46dfd48c
DieCamDu <dcamposd@ucenfotec.ac.cr>
2023-01-09 15:31:08 -0500

    First Commit, calendar app frontend

---
+++ README.md
@@ -0,0 +1,12 @@
+# Calendar APP
+
+## Developer steps
+
+1. Rename file .env.template to .env
+2. Make all the changes on .env file
+
+```
+
+VITE_API_URL=http://localhost:4002/api
+
+```
---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,154 @@
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import { useAuthStore, useForm } from "../../hooks";
+import "./LoginPage.css";
+
+const loginFormFields = {
+  loginEmail: "",
+  loginPassword: "",
+};
+
+const registerFormFields = {
+  registerName: "",
+  registerEmail: "",
+  registerPassword: "",
+  registerPassword2: "",
+};
+
+export const LoginPage = () => {
+  const { startLogin, startRegister, errorMessage } = useAuthStore();
+
+  useEffect(() => {
+    if (errorMessage !== undefined) {
+      Swal.fire("Authentication error", errorMessage, "error");
+    }
+  }, [errorMessage]);
+
+  const {
+    loginEmail,
+    loginPassword,
+    onInputChange: onLoginInputChange,
+  } = useForm(loginFormFields);
+
+  const {
+    registerName,
+    registerEmail,
+    registerPassword,
+    registerPassword2,
+    onInputChange: onRegisterInputChange,
+  } = useForm(registerFormFields);
+
+  const loginSubmit = (event) => {
+    event.preventDefault();
+    startLogin({ email: loginEmail, password: loginPassword });
+  };
+
+  const registerSubmit = (event) => {
+    event.preventDefault();
+    if (registerName.length < 3) {
+      return Swal.fire(
+        "Register error",
+        "Name must have more than 3 characters",
+        "error"
+      );
+    } else if (!registerEmail.includes("@")) {
+      return Swal.fire("Register error", "Email format invalid", "error");
+    } else if (registerPassword !== registerPassword2) {
+      return Swal.fire("Register error", "Password missmatch", "error");
+    }
+    startRegister({
+      name: registerName,
+      email: registerEmail,
+      password: registerPassword,
+    });
+  };
+
+  return (
+    <div className="container login-container">
+      <div className="row">
+        <div className="col-md-6 login-form-1">
+          <h3>Ingreso</h3>
+          <form onSubmit={loginSubmit}>
+            <div className="form-group mb-2">
+              <input
+                autoComplete="off"
+                type="text"
+                className="form-control"
+                placeholder="Email"
+                name="loginEmail"
+                value={loginEmail}
+                onChange={onLoginInputChange}
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                name="loginPassword"
+                value={loginPassword}
+                onChange={onLoginInputChange}
+              />
+            </div>
+            <div className="d-grid gap-2">
+              <input type="submit" className="btnSubmit" value="Login" />
+            </div>
+          </form>
+        </div>
+
+        <div className="col-md-6 login-form-2">
+          <h3>Registro</h3>
+          <form onSubmit={registerSubmit}>
+            <div className="form-group mb-2">
+              <input
+                autoComplete="off"
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="registerName"
+                value={registerName}
+                onChange={onRegisterInputChange}
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                autoComplete="off"
+                type="text"
+                className="form-control"
+                placeholder="Email"
+                name="registerEmail"
+                value={registerEmail}
+                onChange={onRegisterInputChange}
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                name="registerPassword"
+                value={registerPassword}
+                onChange={onRegisterInputChange}
+              />
+            </div>
+
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Repeat Password"
+                name="registerPassword2"
+                value={registerPassword2}
+                onChange={onRegisterInputChange}
+              />
+            </div>
+
+            <div className="d-grid gap-2">
+              <input type="submit" className="btnSubmit" value="Crear cuenta" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
